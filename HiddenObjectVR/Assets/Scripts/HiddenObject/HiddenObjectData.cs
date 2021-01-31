6ddbbaa087ffdc056b6de48586186fa32419fa48
Bradley Adams <brad@bradleyadams.com>
2021-01-31 13:51:27 -0800

    Added objects

--- HiddenObjectVR/Assets/Scripts/HiddenObject/HiddenObjectData.cs
+++ HiddenObjectVR/Assets/Scripts/HiddenObject/HiddenObjectData.cs
@@ -5,6 +5,8 @@ namespace EmeraldActivities
     [CreateAssetMenu(menuName = "Emerald Activities/Hidden Object Data")]
     public class HiddenObjectData : ScriptableObject
     {
-        
+        [SerializeField]
+        private Color _color;
+        public Color Color => _color;
     }
 }
